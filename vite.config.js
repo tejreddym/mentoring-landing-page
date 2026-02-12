702babeb913c2b2aa95b8e92bae1db1319be0c81
tejreddym <tejareddy346834@gmail.com>
2026-02-12 17:53:03 +0530

    Fix: Remove basicSsl and add netlify.toml for production build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: true
   }
